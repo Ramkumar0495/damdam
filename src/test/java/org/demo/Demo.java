efc639cf3f57a20ab0e1359727efb3c617dc0d53
Ramkumar <ramkumar0495@gmail.com>
2023-03-24 14:56:44 +0530

    login

---
+++ src/test/java/org/demo/Demo.java
@@ -0,0 +1,15 @@
+package org.demo;
+
+public class Demo {
+ 
+	private void tc1() {
+       System.out.println("tc1");
+	}
+	private void tc2() {
+		System.out.println("tc2");
+	}
+	private void tc3() {
+		System.out.println("tc3");
+	}
+	
+}
